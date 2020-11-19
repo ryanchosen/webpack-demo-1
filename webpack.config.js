1d7fcf63645eb84d356d28138a08710356d8b0e5
RyanSu <563607356@qq.com>
2020-11-19 12:52:51 +0800

    222

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 module.exports={
     mode: "development",
     entry:'./src/index.js',
     output:{
         filename: '[name].[contenthash].js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({title: '我就试试而已啦啦啦',template: './src/test.html'})],
 }
+
+
