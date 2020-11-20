849594ddecfa9b60dc391ea49f44c184b169c834
RyanSu <563607356@qq.com>
2020-11-20 16:11:43 +0800

    main update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import windowPng from './1.png' //
 
 console.log(windowPng) // 4c29e74ae32d270769c4b3fb8699c83f.png  看起来是文件名，实际上得到的是路径而不是内容
 let app = document.querySelector('#app');
-app.innerHTML = `<img src="${windowPng}">`
+// app.innerHTML = `<img src="${windowPng}">`
 
 let btn = document.createElement('button')
 btn.innerText = '懒加载'
